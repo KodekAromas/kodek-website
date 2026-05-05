d575e5d198dd326aef0c59ebac35feeed97aabb7
Soumya Darshan Pattanaik <admin@kodek.in>
2026-05-05 11:04:50 +0530

    Fixed Chat kody and Responsiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
   compressHTML: true,
 });
